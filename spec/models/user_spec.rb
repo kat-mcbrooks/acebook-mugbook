2000e2bce7c473793300c7ea8ff59d4604b0a772
icvotria <icvotria@gmail.com>
2022-02-08 14:59:57 +0000

    model - create user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,3 +3,13 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe User do
+  user = User.new()
+  it 'should create a new user with username, email, password properties' do
+    user.username = 'Victoria'
+    user.email = 'test@test.com'
+    user.password = '123456'
+    expect(user.username).to eq 'Victoria'
+  end
+end
