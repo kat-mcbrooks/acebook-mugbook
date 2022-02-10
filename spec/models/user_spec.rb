0b78edd7f700439b5a1eceddfe133bef3a735748
icvotria <icvotria@gmail.com>
2022-02-10 10:59:22 +0000

    tiny bit of linting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,26 +4,26 @@ describe User do
   it { should validate_email_format_of(:email).with_message('That\'s not an email!') }
   
   it 'is invalid if email is incorrect format' do
-    expect(User.new({username: 'username', name: 'name', password: 'password', email: 'not_an_email'}).valid?).to be_falsey
+    expect(User.new({ username: 'username', name: 'name', password: 'password', email: 'not_an_email' }).valid?).to be_falsey
   end
   
   it 'is invalid if password is too short' do
-    expect(User.new({username: 'username', name: 'name', password: 'pass', email: 'e@mail'}).valid?).to be_falsey
+    expect(User.new({ username: 'username', name: 'name', password: 'pass', email: 'e@mail' }).valid?).to be_falsey
   end
   
   it 'is invalid if username does not exist' do
-    expect(User.new({username: nil, name: 'name', password: 'password', email: 'e@mail'}).valid?).to be_falsey
+    expect(User.new({ username: nil, name: 'name', password: 'password', email: 'e@mail' }).valid?).to be_falsey
   end
   
   it 'is invalid if name does not exist' do
-    expect(User.new({username: 'username', name: nil, password: 'password', email: 'e@mail'}).valid?).to be_falsey
+    expect(User.new({ username: 'username', name: nil, password: 'password', email: 'e@mail' }).valid?).to be_falsey
   end
   
   it 'is invalid if password does not exist' do
-    expect(User.new({username: 'username', name: 'name', password: nil, email: 'e@mail'}).valid?).to be_falsey
+    expect(User.new({ username: 'username', name: 'name', password: nil, email: 'e@mail' }).valid?).to be_falsey
   end
   
   it 'is invalid if name does not exist' do
-    expect(User.new({username: 'username', name: 'name', password: 'password', email: nil}).valid?).to be_falsey
+    expect(User.new({ username: 'username', name: 'name', password: 'password', email: nil }).valid?).to be_falsey
   end
 end
