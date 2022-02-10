04b1f4ef5b05bcf34bcfc22e2cf75bd6bca316bb
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-02-10 14:31:15 +0000

    users_controller_test

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe 'GET /new ' do
+    it 'responds with 200' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'POST /' do
+    it 'responds with 200' do
+      post :create, params: { user: { name: 'name', username: 'username', email: 'email@test.com', password: 'password' } }
+      expect(response).to redirect_to(posts_url)
+    end
+
+    it 'creates a user' do
+      post :create, params: { user: { name: 'name', username: 'username', email: 'email@test.com', password: 'password' } }
+      expect(User.find_by(email: 'email@test.com')).to be
+    end
+
+    it 'deletes a user' do
+      post :create, params: { user: { name: 'name', username: 'username', email: 'email@test.com', password: 'password' } }
+      delete :destroy(@user[:id])
+      expect(User.find_by(id: @user)).to not_be
+    end
+  end
+
+  describe 'GET /' do
+    it 'responds with 200' do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  end
+end
