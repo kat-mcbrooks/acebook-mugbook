ec85aae92d042bbd3057da9c4f6e3c02c6c09224
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-02-14 17:46:06 +0000

    setup homepage, install font-awesome v4 and add mug icon. bootstrap v4 working.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_135937) do
+ActiveRecord::Schema.define(version: 2022_02_14_135633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,19 +19,6 @@ ActiveRecord::Schema.define(version: 2022_02_14_135937) do
     t.string "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.text "name"
-    t.text "username"
-    t.text "email"
-    t.text "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
-  end
-
-  add_foreign_key "posts", "users"
 end
