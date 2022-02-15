3c843d18787743ffb847550279ae39aeb2643c15
icvotria <icvotria@gmail.com>
2022-02-15 16:40:38 +0000

    sorting out post/user stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,7 @@ class UsersController < ApplicationController
    end
    private
    def user_params
-     params.require(:user).permit(:username, :name, :website,
-                                  :bio, :email, :phone, :gender)
+     params.require(:user).permit(:username, :email)
    end
  
  end
