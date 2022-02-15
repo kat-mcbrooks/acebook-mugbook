f408a1be2e5b153dbf6de83007f863415c0d1f6c
icvotria <icvotria@gmail.com>
2022-02-15 15:38:57 +0000

    accidentally changed uc, so changed it back

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,99 +1,20 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-  before_action :set_user, only: %i[ show edit update destroy ]
-
-  # GET /users or /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1 or /users/1.json
   def show
+     
   end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
+ 
   def edit
+   @user = User.find(params[:id])
   end
-
-  # POST /users or /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        session[:user_id] = @user.id
-
-        format.html { redirect_to posts_url, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /users/1 or /users/1.json
+ 
   def update
-    puts 'update'
-    respond_to do |format|
-      puts 'respond block'
-      if @user.update(user_params)
-        puts 'success'
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        puts 'failure'
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1 or /users/1.json
-  def destroy
-    p 'rspec check'
-    @user.destroy
-
-    respond_to do |format|
-      p 'respond block'
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
-    end
-=======
- def show
-    
+     current_user.update(user_params)
+     redirect_to current_user
+   end
+   private
+   def user_params
+     params.require(:user).permit(:username, :name, :website,
+                                  :bio, :email, :phone, :gender)
+   end
+ 
  end
-
- def edit
-  @user = User.find(params[:id])
- end
-
- def update
-    current_user.update(user_params)
-    redirect_to current_user
-  end
-  private
-  def user_params
-    params.require(:user).permit(:username, :name, :website,
-                                 :bio, :email, :phone, :gender)
-  end
-
->>>>>>> e2f6052bb3b1c991046f44fbcbe1eb2313eef9f8
-end
