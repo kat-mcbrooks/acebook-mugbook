0fef19ceb98ea868ab94cd7cc3b28acec98412bd
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-02-15 12:42:44 +0000

    profile page is exist but empty

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
   #root "posts#index"
   root 'home#index'
-    
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
