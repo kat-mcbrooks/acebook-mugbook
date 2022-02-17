e2312dc7dba46ad752a83f1b1649569f65fa0dea
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-02-17 10:50:41 +0000

    changes to views and some profile edits stuff e.g. password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_one_attached:image
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
