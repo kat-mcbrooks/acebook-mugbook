bf94072d64e5b9e6f38655658ef323d5c749309b
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-02-17 17:07:37 +0000

    edit profile links working. images nearly attached to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,8 @@ class PostsController < ApplicationController
 
   def index
     @post = Post.new
-    @posts = Post.all.order('created_at DESC')
+    @posts = Post.all.with_attached_images.order('created_at DESC')
+    # @posts.map {|post| post.images.attach(params[:images])}
   end
 
   def new
@@ -16,6 +17,8 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create!(post_params)
+    @post.images.attach(params[:post][:images])
+   #if you change to one image upload only, the above line isn't needed
 
     respond_to do |format|
       if @post.save
@@ -71,6 +74,7 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:message, :user_id)
+    params.require(:post).permit(:message, :user_id, images: [])
+    # params.require(:post).permit(:message, :user_id, :image)
   end
 end
